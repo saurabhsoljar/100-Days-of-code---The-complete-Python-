dc7f9a427e6e58d3d89e202662fa847bf695becf
saurabh kumar <93548806+saurabhsoljar@users.noreply.github.com>
2026-07-18 23:35:56 +0530

    delete print name

--- Day-18/PythonProject2/quiz-game-start/quiz_brain.py
+++ Day-18/PythonProject2/quiz-game-start/quiz_brain.py
@@ -13,6 +13,7 @@ class QuizBrain:
         self.question_number += 1
         user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False):")
         self.check_answer(user_answer, current_question.answer)
+        
 
 def check_answer(self,user_answer,correct_answer):
     if user_answer.lower() == correct_answer.lower():
@@ -23,4 +24,3 @@ def check_answer(self,user_answer,correct_answer):
     print(f"The correct answer was {correct_answer}")
     print((f"Your current score is: {self.score}/{self.question_number}"))
     print("\n")
-    print("saurabh")
